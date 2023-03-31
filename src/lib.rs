c6e2039d69f892d2f3477b74fca67ba6ea5a7d84
Antonio Leitao <aml.leitao@campus.fct.unl.pt>
2023-03-31 16:57:47 +0200

    becutifull first working example, maturin and pyo3 and freaking awesome

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,42 @@
 use pyo3::prelude::*;
-use rand::Rng;
-use std::cmp::Ordering;
-use std::io;
+use hashbrown::HashMap;
 
-#[pyfunction]
-fn guess_the_number() {
-    println!("Guess the number!");
+#[pyclass(name = "VLMC")]
+struct VLMCObject {
+    max_depth: usize,
+    alphabet_size: usize,
+    #[pyo3(get)]
+    counts: usize 
+}
 
-    let secret_number = rand::thread_rng().gen_range(1..101);
+#[pymethods]
+impl VLMCObject {
+    #[new]
+    #[pyo3(signature = (max_depth, alphabet_size))]
+    fn new(max_depth: usize,alphabet_size: usize) -> Self {
+        VLMCObject { max_depth: max_depth, alphabet_size: alphabet_size, counts: 0}
+    }
+    #[pyo3(signature = (data))]
+    fn fit(&mut self, data: Vec<Vec<u32>>) -> PyResult<()> {
+        self.counts = count_sequences(data, self.max_depth);
+        Ok(())
+    }
+}
 
-    loop {
-        println!("Please input your guess.");
 
-        let mut guess = String::new();
 
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        println!("You guessed: {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
+//#[pyfunction]
+fn count_sequences(vectors: Vec<Vec<u32>>, n: usize) -> usize {
+    let mut counts = HashMap::new();
+    for vector in vectors {
+        for i in 0..vector.len() {
+            for j in i..std::cmp::min(i + n, vector.len()) {
+                let sequence = &vector[i..j+1];
+                *counts.entry(sequence.to_vec()).or_insert(0) += 1;
             }
         }
     }
+    counts.into_values().sum()
 }
 
 /// A Python module implemented in Rust. The name of this function must match
@@ -41,7 +44,8 @@ fn guess_the_number() {
 /// import the module.
 #[pymodule]
 fn vlmc(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(guess_the_number, m)?)?;
+    //m.add_function(wrap_pyfunction!(count_sequences, m)?)?;
+    m.add_class::<VLMCObject>()?;
 
     Ok(())
 }
