dd4aace20ecf0b8ba80d7701dc3fc5738d96197a
Antonio Leitao <aml.leitao@campus.fct.unl.pt>
2023-04-03 19:58:27 +0200

    closing for the day

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,190 @@
-use hashbrown::HashMap;
+use hashbrown::{HashMap, HashSet};
 use pyo3::prelude::*;
+use std::thread;
 
+//------------------ STRUCTURES ---------------------
+#[derive(Debug, PartialEq)]
+struct Node {
+    count: usize,
+    precedents: HashSet<u32>,
+    successors: HashSet<u32>,
+}
+impl Node {
+    fn new() -> Self {
+        Node {
+            count: 0,
+            precedents: HashSet::new(),
+            successors: HashSet::new(),
+        }
+    }
+}
+
+enum NumJobs {
+    One,
+    Many(usize),
+}
+
+impl NumJobs {
+    fn from_i32(n_jobs: i32) -> NumJobs {
+        if n_jobs == 1 {
+            NumJobs::One
+        } else if n_jobs > 1 {
+            NumJobs::Many(n_jobs as usize)
+        } else {
+            NumJobs::Many(num_cpus::get())
+        }
+    }
+}
+//------------------------------------------------------
+
+//------------------ AUX FUNCTIONS ---------------------
+fn count_subsequences_parallel(
+    data: Vec<Vec<u32>>,
+    max_depth: usize,
+    num_threads: usize,
+) -> HashMap<Vec<u32>, Node> {
+    //let num_threads = num_cpus::get(); // get the number of available CPU cores
+    let chunk_size = data.len() / num_threads; // divide the data into equal-sized chunks
+    let mut threads = Vec::with_capacity(num_threads);
+
+    // spawn threads to process each chunk
+    for chunk in data.chunks(chunk_size) {
+        let chunk_data = chunk.to_vec(); // convert the chunk slice to a vector
+
+        let handle = thread::spawn(move || count_subsequences(chunk_data, max_depth));
+
+        threads.push(handle);
+    }
+
+    // collect the results from all threads and combine them into a single HashMap
+    let mut counts = HashMap::new();
+
+    for handle in threads {
+        let chunk_counts = handle.join().unwrap();
+        for (k, v) in chunk_counts {
+            let node = counts.entry(k).or_insert(Node::new());
+            node.count += v.count;
+            node.precedents.extend(v.precedents);
+            node.successors.extend(v.successors);
+        }
+    }
+
+    counts
+}
+
+fn count_subsequences(data: Vec<Vec<u32>>, max_depth: usize) -> HashMap<Vec<u32>, Node> {
+    let mut counts: HashMap<Vec<u32>, Node> = HashMap::new();
+    for seq in data {
+        let n = seq.len().min(max_depth);
+
+        for i in 0..seq.len() {
+            for j in i..(i + n).min(seq.len()) {
+                let subseq = &seq[i..=j];
+                let node = counts.entry(subseq.to_vec()).or_insert(Node::new());
+                node.count += 1;
+
+                if i > 0 {
+                    node.precedents.insert(seq[i - 1]);
+                }
+                if j < seq.len() - 1 {
+                    node.successors.insert(seq[j + 1]);
+                }
+            }
+        }
+    }
+    counts
+}
+//------------------------------------------------------
+
+
+//-------------------- MAIN CLASS ----------------------
 #[pyclass(name = "VLMC")]
 struct VLMCObject {
     max_depth: usize,
     alphabet_size: usize,
+    n_jobs: NumJobs,
     #[pyo3(get)]
     counts: usize,
+    nodes: HashMap<Vec<u32>,Node>
 }
 
 #[pymethods]
 impl VLMCObject {
     #[new]
-    #[pyo3(signature = (max_depth, alphabet_size))]
-    fn new(max_depth: usize, alphabet_size: usize) -> Self {
+    #[pyo3(signature = (max_depth, alphabet_size,n_jobs=-1))]
+    fn new(max_depth: usize, alphabet_size: usize, n_jobs: i32) -> Self {
         VLMCObject {
             max_depth: max_depth,
             alphabet_size: alphabet_size,
+            n_jobs: NumJobs::from_i32(n_jobs),
             counts: 0,
+            nodes : HashMap::new()
         }
     }
+
+    //THIS ONE IS IMPORTANT NOT JUST FOR SKLEARN API BUT ALSO
+    // TO AVOID UNNECESSARY CHECKS DOWN THE LINE.
+    fn check_params(&self){
+    }
+
+
     #[pyo3(signature = (data))]
     fn fit(&mut self, data: Vec<Vec<u32>>) -> PyResult<()> {
-        self.counts = count_sequences(data, self.max_depth);
-        Ok(())
-    }
-}
-
-//#[pyfunction]
-fn count_sequences(vectors: Vec<Vec<u32>>, n: usize) -> usize {
-    let mut counts = HashMap::new();
-    for vector in vectors {
-        for i in 0..vector.len() {
-            for j in i..std::cmp::min(i + n, vector.len()) {
-                let sequence = &vector[i..j + 1];
-                *counts.entry(sequence.to_vec()).or_insert(0) += 1;
+        match self.n_jobs {
+            NumJobs::One => {
+                self.counts = count_subsequences(data, self.max_depth)
+                    .into_values()
+                    .map(|node| node.count)
+                    .sum();
+            }
+            NumJobs::Many(num_jobs) => {
+                self.counts = count_subsequences_parallel(data, self.max_depth, num_jobs)
+                    .into_values()
+                    .map(|node| node.count)
+                    .sum();
             }
         }
+
+        Ok(())
     }
-    counts.into_values().sum()
+    #[pyo3(signature = (data))]
+    fn fit_transform(&mut self, data: Vec<Vec<u32>>) -> PyResult<()> {
+        match self.n_jobs {
+            NumJobs::One => {
+                self.nodes = count_subsequences(data, self.max_depth);
+            }
+            NumJobs::Many(num_jobs) => {
+                self.nodes = count_subsequences_parallel(data, self.max_depth, num_jobs);
+            }
+        }
+        Ok(())
+    }
+
+
+
+    fn get_suffix(&self, sequence: Vec<u32>) -> &Node{
+        let mut sequence = sequence;
+        while !sequence.is_empty() {
+            match self.nodes.get(&sequence){
+                Some(node)=> {return node},
+                None => sequence.remove(0)
+            };
+        }
+        self.nodes.get(&vec![]).unwrap()
+    }
+
+    fn get_distribution(&self, sequence: Vec<u32>)->Vec<u32>{
+        let distribution:Vec<u32> = vec![0; self.alphabet_size];
+        let node = self.nodes.get(&sequence).unwrap();
+        for successor in node.successors{
+
+        }
+        discribution
+    }
+
 }
+//------------------------------------------------------
+
 
 /// A Python module implemented in Rust. The name of this function must match
 /// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
--- src/main.rs
+++ src/main.rs
@@ -1,107 +1,98 @@
-use hashbrown::{HashMap, HashSet};
-use std::thread;
-use rand::Rng;
-use std::time::Instant;
+// use hashbrown::{HashMap, HashSet};
+// use rand::Rng;
+// use std::thread;
+// use std::time::Instant;
 
-fn count_subsequences_parallel(data: Vec<Vec<u32>>, max_depth: usize) -> HashMap<Vec<u32>, (usize, HashSet<u32>)> {
-    let num_threads = num_cpus::get(); // get the number of available CPU cores
-    let chunk_size = data.len() / num_threads; // divide the data into equal-sized chunks
-    let mut threads = Vec::with_capacity(num_threads);
-
-    // spawn threads to process each chunk
-    for chunk in data.chunks(chunk_size) {
-        let chunk_data = chunk.to_vec(); // convert the chunk slice to a vector
-
-        let handle = thread::spawn(move || {
-            count_subsequences(chunk_data, max_depth)
-        });
-
-        threads.push(handle);
-    }
-
-    // collect the results from all threads and combine them into a single HashMap
-    let mut counts = HashMap::new();
-
-    for handle in threads {
-        let chunk_counts = handle.join().unwrap();
-        for (k, v) in chunk_counts {
-            let count = counts.entry(k).or_insert((0, HashSet::new()));
-            count.0 += v.0;
-            count.1.extend(v.1);
-        }
-    }
-
-    counts
-}
-
-struct Node {
-    count: usize,
-    precedents: HashSet<u32>,
-}
-
-fn count_subsequences(data:Vec<Vec<u32>>,max_depth:usize)->HashMap<Vec<u32>,(usize,HashSet<u32>)>{
-    let mut counts: HashMap<Vec<u32>, (usize, HashSet<u32>)> = HashMap::new();
-    for seq in data {
-        let n = seq.len().min(max_depth);
-
-        for i in 0..seq.len() {
-            for j in i..(i + n).min(seq.len()) {
-                let subseq = &seq[i..=j];
-                let count = counts.entry(subseq.to_vec()).or_insert((0, HashSet::new()));
-                count.0 += 1;
-
-                if i > 0 {
-                    count.1.insert(seq[i-1]);
-                   
-                }
-            }
-        }
-    }
-    counts
-}
-
-// fn main() {
-//     let data = vec![
-//         vec![1, 2, 3, 4],
-//         vec![2, 3, 4, 5],
-//         vec![1, 2, 3, 5],
-//         vec![1, 2],
-//     ];
-//     let max_depth = 3;
-//    let counts = count_subsequences(data, max_depth);
-//     for (subseq, (count, precedents)) in counts {
-//         println!("Subsequence {:?} has count {} and precedents {:?}", subseq, count, precedents);
+// #[derive(Debug, PartialEq)]
+// struct Node {
+//     count: usize,
+//     precedents: HashSet<u32>,
+// }
+// impl Node {
+//     fn new() -> Self {
+//         Node {
+//             count: 0,
+//             precedents: HashSet::new(),
+//         }
 //     }
 // }
 
+// fn count_subsequences_parallel(data: Vec<Vec<u32>>, max_depth: usize) -> HashMap<Vec<u32>, Node> {
+//     let num_threads = num_cpus::get(); // get the number of available CPU cores
+//     let chunk_size = data.len() / num_threads; // divide the data into equal-sized chunks
+//     let mut threads = Vec::with_capacity(num_threads);
 
-fn main() {
-    let mut rng = rand::thread_rng();
-    let data: Vec<Vec<u32>> = (0..1000)
-        .map(|_| (0..100).map(|_| rng.gen_range(0..10)).collect())
-        .collect();
+//     // spawn threads to process each chunk
+//     for chunk in data.chunks(chunk_size) {
+//         let chunk_data = chunk.to_vec(); // convert the chunk slice to a vector
 
-    let start_time = Instant::now();
-    let counts = count_subsequences(data.clone(), 5);
-    let end_time = Instant::now();
-    println!(
-        "Sequential execution time: {} ms",
-        (end_time - start_time).as_millis()
-    );
+//         let handle = thread::spawn(move || count_subsequences(chunk_data, max_depth));
 
-    let start_time = Instant::now();
-    let counts_parallel = count_subsequences_parallel(data.clone(), 5);
-    let end_time = Instant::now();
-    println!(
-        "Parallel execution time: {} ms",
-        (end_time - start_time).as_millis()
-    );
-    assert_eq!(counts, counts_parallel);
-    let sequential_sum:usize=counts.into_values().map(|(counts,_)|counts).sum();
-    let parallel_sum:usize=counts_parallel.into_values().map(|(counts,_)|counts).sum();
+//         threads.push(handle);
+//     }
 
+//     // collect the results from all threads and combine them into a single HashMap
+//     let mut counts = HashMap::new();
 
-    
-    assert_eq!(sequential_sum, parallel_sum);
+//     for handle in threads {
+//         let chunk_counts = handle.join().unwrap();
+//         for (k, v) in chunk_counts {
+//             let node = counts.entry(k).or_insert(Node::new());
+//             node.count += v.count;
+//             node.precedents.extend(v.precedents);
+//         }
+//     }
 
-}
+//     counts
+// }
+
+// fn count_subsequences(data: Vec<Vec<u32>>, max_depth: usize) -> HashMap<Vec<u32>, Node> {
+//     let mut counts: HashMap<Vec<u32>, Node> = HashMap::new();
+//     for seq in data {
+//         let n = seq.len().min(max_depth);
+
+//         for i in 0..seq.len() {
+//             for j in i..(i + n).min(seq.len()) {
+//                 let subseq = &seq[i..=j];
+//                 let node = counts.entry(subseq.to_vec()).or_insert(Node::new());
+//                 node.count += 1;
+
+//                 if i > 0 {
+//                     node.precedents.insert(seq[i - 1]);
+//                 }
+//             }
+//         }
+//     }
+//     counts
+// }
+
+// fn main() {
+//     let mut rng = rand::thread_rng();
+//     let data: Vec<Vec<u32>> = (0..10000)
+//         .map(|_| (0..100).map(|_| rng.gen_range(0..10)).collect())
+//         .collect();
+//     let max_depth:usize = 10;
+
+//     let start_time = Instant::now();
+//     let counts = count_subsequences(data.clone(), max_depth);
+//     let end_time = Instant::now();
+//     println!(
+//         "Sequential execution time: {} ms",
+//         (end_time - start_time).as_millis()
+//     );
+
+//     let start_time = Instant::now();
+//     let counts_parallel = count_subsequences_parallel(data.clone(), max_depth);
+//     let end_time = Instant::now();
+//     println!(
+//         "Parallel execution time: {} ms",
+//         (end_time - start_time).as_millis()
+//     );
+
+//     assert_eq!(counts, counts_parallel);
+
+//     let sequential_sum: usize = counts.into_values().map(|node| node.count).sum();
+//     let parallel_sum: usize = counts_parallel.into_values().map(|node| node.count).sum();
+
+//     assert_eq!(sequential_sum, parallel_sum);
+// }
