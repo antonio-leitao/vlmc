3735d4a5f02d534394b640b81e1474a98a5eb489
Antonio Leitao <aml.leitao@campus.fct.unl.pt>
2023-06-05 22:07:06 +0200

    Finished code refactoring. moving on to testing

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,7 @@
 use hashbrown::{HashMap, HashSet};
 use pyo3::prelude::*;
-//use std::collections::VecDeque;
 //use std::thread;
 mod peres_shield;
-use self::peres_shield::peres_shield_prunning;
 
 //###################### PYTHON INTERFACE ##########################
 //Anything inside this section is exposed to python
@@ -20,43 +18,144 @@ fn vlmc(_py: Python, m: &PyModule) -> PyResult<()> {
 //--------------------------- MAIN CLASS ----------------------
 #[pyclass(name = "VLMC")]
 pub struct VLMCObject {
-    counts: HashMap<String, u32>,
+    nodes: HashMap<Vec<usize>, Node>,
+    alphabet_size: usize,
+    max_depth: usize,
+    total_symbols: u32,
 }
 
 #[pymethods]
 impl VLMCObject {
     #[new]
-    fn new() -> Self {
+    #[pyo3(signature = (alphabet_size, max_depth=10))]
+    fn new(alphabet_size: usize, max_depth: usize) -> Self {
         VLMCObject {
-            counts: HashMap::new(),
+            nodes: HashMap::new(),
+            alphabet_size: alphabet_size,
+            max_depth: max_depth,
+            total_symbols: 0,
         }
     }
 
     #[pyo3(signature = (data))]
-    fn fit(&mut self, data: Vec<&str>) -> PyResult<()> {
+    fn fit(&mut self, data: Vec<Vec<usize>>) -> PyResult<()> {
         self._fit(data);
         Ok(())
+    }
+    #[pyo3(signature = (sequence))]
+    fn get_suffix(&self, sequence: Vec<usize>) -> Vec<usize> {
+        self._suffix(&sequence)
+    }
+    #[pyo3(signature = (sequence))]
+    fn get_counts(&self, sequence: Vec<usize>) -> u32 {
+        self._counts(&sequence)
+    }
+    #[pyo3(signature = (sequence))]
+    fn get_distribution(&self, sequence: Vec<usize>) -> Vec<u32> {
+        self._distribution(&sequence).to_vec()
     }
 }
 
 //############################# BACKEND ##########################
 //Here should be methods and stuff that will not be exposed to Python
-
 impl VLMCObject {
-    fn _fit(&mut self, data: Vec<&str>) {
+    fn _fit(&mut self, data: Vec<Vec<usize>>) {
         //creates counts
-        let mut raw_counts: HashMap<String, u32> = HashMap::new();
-        for string in data {
-            *raw_counts.entry(string.to_string()).or_insert(0) += 1;
+        let raw_counts = count_subsequences(data, self.alphabet_size, self.max_depth);
+        self.nodes = raw_counts;
+        self.total_symbols = self.nodes.get(&vec![]).unwrap().count;
+        //prunes
+        self.peres_shield_prune();
+    }
+    fn _suffix(&self, sequence: &Vec<usize>) -> Vec<usize> {
+        let mut suffix: &[usize] = sequence;
+        while !suffix.is_empty() && !self.nodes.contains_key(suffix) {
+            suffix = &suffix[1..];
         }
-        //prunes counts
-        self.counts = peres_shield_prunning(raw_counts);
+        suffix.to_vec()
+    }
+    fn _node(&self, sequence: &Vec<usize>) -> &Node {
+        let suffix = self._suffix(sequence);
+        self.nodes.get(&suffix).unwrap()
+    }
+    fn _counts(&self, sequence: &Vec<usize>) -> u32 {
+        let node = self._node(sequence);
+        node.count
+    }
+    fn _distribution(&self, sequence: &Vec<usize>) -> &Vec<u32> {
+        let node = self._node(sequence);
+        &node.distribution
     }
 }
+
 #[derive(Debug, PartialEq)]
 struct Node {
-    count: usize,
-    precedents: HashSet<u32>,
-    successors: HashSet<u32>,
+    count: u32,
+    precedents: HashSet<usize>,
+    successors: HashSet<usize>,
     distribution: Vec<u32>,
+}
+impl Node {
+    fn new(alphabet_size: usize) -> Self {
+        Node {
+            count: 0,
+            precedents: HashSet::new(),
+            successors: HashSet::new(),
+            distribution: vec![0; alphabet_size],
+        }
+    }
+    fn predecessor_sequences(&self, key: &[usize]) -> Vec<Vec<usize>> {
+        let mut pred_seqs = Vec::new();
+        for pred_key in &self.precedents {
+            let mut pred_seq = vec![*pred_key];
+            pred_seq.extend_from_slice(key);
+            pred_seqs.push(pred_seq);
+        }
+        pred_seqs
+    }
+}
+
+fn count_subsequences(
+    data: Vec<Vec<usize>>,
+    alphabet_size: usize,
+    max_depth: usize,
+) -> HashMap<Vec<usize>, Node> {
+    let mut counts: HashMap<Vec<usize>, Node> = HashMap::new();
+    let mut zero_counts: u32 = 0;
+    let mut zero_successors: HashSet<usize> = HashSet::new();
+    let mut zero_distribution: Vec<u32> = vec![0; alphabet_size];
+    for seq in data {
+        zero_counts += seq.len() as u32;
+        let n = seq.len().min(max_depth);
+        for i in 0..seq.len() {
+            //update root node
+            zero_successors.insert(seq[i]);
+            zero_distribution[seq[i]] += 1;
+            //update higher order memory
+            for j in i..(i + n).min(seq.len()) {
+                let subseq = &seq[i..=j];
+                let node = counts
+                    .entry(subseq.to_vec())
+                    .or_insert(Node::new(alphabet_size));
+                node.count += 1;
+
+                if i > 0 {
+                    node.precedents.insert(seq[i - 1]);
+                }
+                if j < seq.len() - 1 {
+                    let element = seq[j + 1];
+                    node.successors.insert(element);
+                    node.distribution[element] += 1
+                }
+            }
+        }
+    }
+    let zero_node = Node {
+        count: zero_counts,
+        precedents: HashSet::new(),
+        successors: zero_successors,
+        distribution: zero_distribution,
+    };
+    counts.insert(vec![], zero_node);
+    counts
 }
--- src/peres_shield.rs
+++ src/peres_shield.rs
@@ -1,9 +1,90 @@
-use hashbrown::HashMap;
+use crate::VLMCObject; // Import the struct from lib.rs
+use hashbrown::HashSet;
+use std::collections::VecDeque;
 
-pub fn peres_shield_prunning(mut counts: HashMap<String,u32>) -> HashMap<String,u32>{
-    // Implementation logic for counting subsequences
-    // ...
-    // Return the count
-    *counts.entry("main".to_string()).or_insert(0) += 1;
-    counts
+impl VLMCObject {
+    // Define additional methods here
+    pub fn peres_shield_prune(&mut self) {
+        //threshold parameters
+        let maximum_extension = (self.total_symbols as f32).log(10.0).log(10.0) as usize;
+        let divergence_threshold = (self.total_symbols as f32).powf(3.0 / 4.0);
+        //instantiate data structures
+        let mut definitive_seqs = HashSet::new();
+        let mut queue = VecDeque::new();
+        //Add the root node
+        definitive_seqs.insert(vec![]);
+        queue.push_back(vec![]);
+
+        //iterate top-down
+        while let Some(parent_sequence) = queue.pop_front() {
+            //stop if depth exceeded;
+            if parent_sequence.len() == self.max_depth {
+                continue;
+            }
+            //expand breath first on each node
+            for new_parent in self.peres_shield_extend_sequence(
+                parent_sequence,
+                maximum_extension,
+                divergence_threshold,
+            ) {
+                //if it has been already evaluated move on
+                if !definitive_seqs.insert(new_parent.clone()) {
+                    continue;
+                }
+                //else add it to the queue
+                queue.push_back(new_parent);
+            }
+        }
+        //discard all but the ones we selected
+        self.nodes.retain(|key, _| definitive_seqs.contains(key));
+    }
+    fn peres_shield_extend_sequence(
+        &self,
+        parent_key: Vec<usize>,
+        maximum_extension: usize,
+        divergence_threshold: f32,
+    ) -> Vec<Vec<usize>> {
+        let mut queue = VecDeque::new();
+        let mut accepted_sequences = Vec::new();
+        let maximum_len = self.max_depth.min(parent_key.len() + maximum_extension);
+        queue.push_back(parent_key.clone());
+        while let Some(key) = queue.pop_front() {
+            //go through every tree child
+            for child_key in self.nodes.get(&key).unwrap().predecessor_sequences(&key) {
+                //we have reached the end of the line
+                if child_key.len() > maximum_len {
+                    break;
+                }
+                //accept new node if fluctuation is bigger than some threshold
+                if self.peres_shield_divergence(&parent_key, &child_key) <= divergence_threshold {
+                    continue;
+                }
+                queue.push_back(child_key.clone());
+                accepted_sequences.push(child_key);
+            }
+        }
+        accepted_sequences
+    }
+
+    fn peres_shield_divergence(&self, seq1: &Vec<usize>, seq2: &Vec<usize>) -> f32 {
+        let n_v = self.nodes.get(seq1).unwrap();
+        let n_w = self.nodes.get(seq2).unwrap();
+        let mut max_divergence: f32 = 0.0;
+
+        for successor in n_v.successors.union(&n_w.successors) {
+            let divergence: f32;
+            let n_va = n_v.distribution[*successor];
+            let n_wa = n_w.distribution[*successor];
+            if n_w.count == 0 {
+                divergence = n_w.count as f32;
+            } else {
+                divergence =
+                    (n_va as f32 - (n_wa as f32 * n_v.count as f32 / n_w.count as f32)).abs();
+            }
+            if divergence >= max_divergence {
+                max_divergence = divergence;
+            }
+        }
+        max_divergence
+    }
 }
